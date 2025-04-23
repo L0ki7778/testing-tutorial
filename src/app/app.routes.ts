7140a38ce4674250046955b96253f671947c4aba
L0ki7778 <Terra7778@googlemail.com>
2025-04-23 09:11:52 +0200

    minor changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SimpleUnitTestComponent } from './components/simple-unit-test/simple-unit-test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: SimpleUnitTestComponent
+  }
+];
