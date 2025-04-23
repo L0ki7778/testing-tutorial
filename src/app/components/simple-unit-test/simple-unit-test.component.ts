900326b071534054e997e653e6802cc688e52564
L0ki7778 <Terra7778@googlemail.com>
2025-04-23 09:11:36 +0200

    added http request to simple-unit.component

--- src/app/components/simple-unit-test/simple-unit-test.component.ts
+++ src/app/components/simple-unit-test/simple-unit-test.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpRequestService } from '../../services/http-request.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-simple-unit-test',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './simple-unit-test.component.html',
   styleUrl: './simple-unit-test.component.scss'
 })
 export class SimpleUnitTestComponent {
   name = "Angular";
   version = 19
+
+  pokemonService = inject(HttpRequestService);
+  pokemon: any
+
+  ngOnInit() {
+    this.pokemonService.getPokemon('pikachu').subscribe((response) => {
+      this.pokemon = response
+    });
+
+  }
 
 }
