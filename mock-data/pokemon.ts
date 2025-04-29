2f2783e84faf8381cce698b39356cdf7457332cb
L0ki7778 <Terra7778@googlemail.com>
2025-04-29 12:46:47 +0200

    added mock data

---
+++ mock-data/pokemon.ts
@@ -0,0 +1,22 @@
+export const POKEMON: any = {
+  1: { name: 'Bisasam', type: 'grass' },
+  2: { name: 'Glumanda', type: 'fire' },
+  3: { name: 'Schiggy', type: 'water' },
+  4: { name: 'Pikachu', type: 'electric' },
+  5: { name: 'Raupy', type: 'bug' },
+  6: { name: 'Hornliu', type: 'bug' },
+  7: { name: 'Taubsi', type: 'normal' },
+  8: { name: 'Rattfratz', type: 'normal' },
+  9: { name: 'Habitak', type: 'normal' },
+  10: { name: 'Sandan', type: 'ground' },
+  11: { name: 'Vulpix', type: 'fire' },
+  12: { name: 'Zubat', type: 'poison' },
+  13: { name: 'Paras', type: 'bug' },
+  14: { name: 'Bluzuk', type: 'bug' },
+  15: { name: 'Digda', type: 'ground' },
+  16: { name: 'Mauzi', type: 'normal' },
+  17: { name: 'Enton', type: 'water' },
+  18: { name: 'Menki', type: 'fighting' },
+  19: { name: 'Abra', type: 'psychic' },
+  20: { name: 'Machollo', type: 'fighting' }
+};
