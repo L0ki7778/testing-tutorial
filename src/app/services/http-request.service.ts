d0fd3bb0d2d32ef84e3abbfe672eb0d415f3d1ca
L0ki7778 <Terra7778@googlemail.com>
2025-04-30 16:42:12 +0200

    added get and put methods to http.service

--- src/app/services/http-request.service.ts
+++ src/app/services/http-request.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { LoggerService } from './logger.service';
 import { CalculatorService } from './calculator.service';
 import { map, Observable } from 'rxjs';
+import { Pokemon, PokemonJSON } from '../../../mock-data/pokemon';
 
 @Injectable({
   providedIn: 'root'
@@ -26,9 +27,13 @@ export class HttpRequestService {
     return this.http.get(`${this.api}/v2/pokemon/${name}`)
   }
 
-  getPokemonList(): Observable<any[]> {
+  getPokemonList(): Observable<Pokemon[]> {
     return this.http.get(`${this.api}/v2/pokemon/1?limit=20`).pipe(
       map((res: any) => res['payload'])
     );
+  }
+
+  savePokemon(pokemon: Pokemon) {
+    return this.http.put(`${this.api}/v2/pokemon/3`, pokemon)
   }
 }
