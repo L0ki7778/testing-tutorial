296cffb4f8e47fd31d6cb53d138c2f7d459645ac
L0ki7778 <Terra7778@googlemail.com>
2025-04-30 16:41:23 +0200

    removed http-utilities from simple-unit-test.component

--- src/app/components/simple-unit-test/simple-unit-test.component.spec.ts
+++ src/app/components/simple-unit-test/simple-unit-test.component.spec.ts
@@ -4,38 +4,35 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 // enforcement is possible, see https://angular.dev/guide/testing/code-coverage
 
 import { SimpleUnitTestComponent } from './simple-unit-test.component';
-import { HttpRequestService } from '../../services/http-request.service';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+
+
 
 describe('SimpleUnitTestComponent', () => {
   let component: SimpleUnitTestComponent; // die zu testende Komponente wird hier deklariert und typisiert
-  let fixture: ComponentFixture<SimpleUnitTestComponent>, // die zu testende Komponente wird hier deklariert und mit einem generic typisiert
-    httpService: HttpRequestService,
-    httpTestingController: HttpTestingController
+  let fixture: ComponentFixture<SimpleUnitTestComponent> // die zu testende Komponente wird hier deklariert und mit einem generic typisiert
 
   // fixture = Ein Wrapper-objekt für die Komponente im Test
 
   beforeEach(waitForAsync(() => { //? -->async()=>{}
 
-    httpService = jasmine.createSpyObj('HttpRequestService', ['getPokemon']),
+    const httpService = jasmine.createSpyObj('HttpRequestService', ['getPokemon'])
 
 
-      TestBed.configureTestingModule({
-        // TestBed = ein Objekt, das die Testumgebung zur Verfügung stellt indem es ein Testmodul erstellt
-        imports: [SimpleUnitTestComponent],
-        providers: [
-          { provide: httpService, useValue: httpService },
-          provideHttpClient(),
-          provideHttpClientTesting()
-        ]
-        // imports und config entsprechen einem NgModule
-      })
-        .compileComponents(); // :Promise<void>
+    TestBed.configureTestingModule({
+      // TestBed = ein Objekt, das die Testumgebung zur Verfügung stellt indem es ein Testmodul erstellt
+      imports: [],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: httpService, useValue: httpService }
+      ]
+      // imports und config entsprechen einem NgModule
+    })
+      .compileComponents(); // :Promise<void> <----- async ()
     // .compileComponents() = kompiliert alle asynchronen Komponenten (Komponenten, die eine externe Template- oder Stylesheet-Datei verwenden)
 
-    httpService = TestBed.inject(HttpRequestService);
-    httpTestingController = TestBed.inject(HttpTestingController)
     fixture = TestBed.createComponent(SimpleUnitTestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -43,8 +40,7 @@ describe('SimpleUnitTestComponent', () => {
 
   it('should create', () => {
 
-    console.log(fixture)
-    console.log(component)
+
     expect(component).toBeTruthy();
   });
 
